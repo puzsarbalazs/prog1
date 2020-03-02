0c84584bd2ed1eb3e9c24dce0a0bab3e53c99fb4
puzsarbalazs <61715588+puzsarbalazs@users.noreply.github.com>
2020-03-03 00:02:37 +0100

    Add files via upload

---
+++ vegtelen2.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+            int main(void)
+            {
+                    for(;;)
+                    {
+                    	//printf("fut\n");
+                       
+                    }
+                    return 0;
+            }
